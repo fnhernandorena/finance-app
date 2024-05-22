43ae18d538829dad65a39417bbc8113932185862
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-05-22 01:40:50 -0300

    finance start

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+<div class="flex items-center flex-col">
+  <Header/>
+  <Nav/>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-5xl text-gray-600">
+        <span class="text-sky-700 font-bold">
+            Finance
+        </span>
+        app
+    </h1>
+</template>
